e0b0ffceeffc118a3ca998a57d109e9f7298da19
fazilwork <fazilwork225@gmail.comgit config --global user.email>
2024-08-12 21:06:08 +0500

    first commit

---
+++ test.py
@@ -0,0 +1,62 @@
+#            ПЕРВЫЙ
+# a = int(input("ведите число: "))
+# if a / 2 == 0:
+#     print("число НЕ четное ")
+# elif a / 2 != 0:
+#     print("число  четное ")
+
+#           ВТОРОЙ
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# a = [a,b,c,d,]
+# b = min(a)
+# print(b)
+
+#                   ТРЕТИЙ
+
+# a = input("ведите чтото")
+# b = a[:: - 1]
+# if a == b:
+#     print("слова палин четатам" )
+# else:
+#     print("да норм число ")    
+
+#                  ЧЕТЫРИ
+
+# n = int(input("пиши число"))
+# for a in range(1,n):
+#     f = 0
+#     f = f + 1
+#     b = n + f
+#     n = b
+#     print(b)
+#                ПЯТЬ(не смог)
+
+# n = int(input("вшап")) 
+
+# for a in range(0 , n):
+#     g = g + 1
+#     f = f + 1
+
+#                     ШЕСТЬ
+# a = int(input("ведите первое число"))
+# b = int(input("ведите второй число"))
+# c = int(input("ведите третие число"))
+# d = int(input("ведите четвертый число"))
+# g = [a,b,c,d,]
+# g.sort()
+# print(g)
+
+# #                       СЕМЬ(нет)
+# e = [2,3,5,7,11,13]
+# n = int(input("число"))
+# for a in range(-1 ,n):
+
+#                           ВОСЕМ
+# a = input("пиши слово")
+# b = len(a)
+# print(b)
